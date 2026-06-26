703e4e43503358ddd74d88aff3471e06e7b1fb4f
sletuffe <sylvain@letuffe.org>
2026-06-26 22:52:09 +0200

    Speed up isolation import: bulk COPY + single UPDATE JOIN instead of 1M individual UPDATEs

--- update_isolations_from_topographic_isolation_viefinderpanoramas.txt.sh
+++ update_isolations_from_topographic_isolation_viefinderpanoramas.txt.sh
@@ -4,15 +4,29 @@
 . $(dirname $0)/config.sh
 
 echo "creating the otm_isolation column to hold isolation information of peaks..."
-psql -d $db -c "ALTER TABLE planet_osm_point ADD COLUMN IF NOT EXISTS otm_isolation integer;" 
+psql -d $db -c "ALTER TABLE planet_osm_point ADD COLUMN IF NOT EXISTS otm_isolation integer;"
 
-echo "creating an index on osm_id because osm2pgsql newer versions does no create it anymore, and we need it to update peak isolation..."
-echo "done"
+echo "Downloading isolation data..."
+TMPFILE=$(mktemp)
+wget -q https://geo.dianacht.de/topo/topographic_isolation_viefinderpanoramas.txt -O - \
+    | grep -v '^#' \
+    | awk -F';' '{print $1";"$NF}' > "$TMPFILE"
+echo "$(wc -l < $TMPFILE) peaks to import"
 
-echo "Updating isolation on all peaks and Volcanos..."
-wget -q https://geo.dianacht.de/topo/topographic_isolation_viefinderpanoramas.txt -O - | egrep -v '^#' | sed s/"\([0-9]*;\).*;.*;\([0-9]*\)"/"update planet_osm_point set otm_isolation=\2 where osm_id=\1"/g | psql -q $db
+echo "Bulk import via temp table + single UPDATE JOIN..."
+psql -d $db -v tmpfile="$TMPFILE" << 'ENDSQL'
+CREATE TEMP TABLE _iso_import (osm_id bigint, isolation integer);
+\copy _iso_import FROM :'tmpfile' WITH (FORMAT CSV, DELIMITER ';')
+UPDATE planet_osm_point p
+    SET otm_isolation = i.isolation
+    FROM _iso_import i
+    WHERE p.osm_id = i.osm_id;
+DROP TABLE _iso_import;
+ENDSQL
+
+rm "$TMPFILE"
 echo "done"
 
 echo "Creating index on otm_isolation..."
-psql -d $db -c "CREATE INDEX idx_peaks_isolation ON planet_osm_point (otm_isolation) WHERE \"natural\" IN ('peak', 'volcano') or amenity = 'parking';"
+psql -d $db -c "CREATE INDEX IF NOT EXISTS idx_peaks_isolation ON planet_osm_point (otm_isolation) WHERE \"natural\" IN ('peak', 'volcano') or amenity = 'parking';"
 echo "done"
