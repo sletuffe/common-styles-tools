de32e2b22f27018ebe735a593d6ca7e75a34b674
sletuffe <sylvain@letuffe.org>
2026-06-26 22:37:54 +0200

    Add timestamped progress output to run-everything.sh

--- run-everything.sh
+++ run-everything.sh
@@ -3,13 +3,36 @@
 cd $(dirname $0)
 . $(dirname $0)/config.sh
 
+log() { echo "[$(date '+%H:%M:%S')] $*"; }
 
+log "=== run-everything START ==="
 
+log "arealabel.sql..."
 cat arealabel.sql | psql $db
-cat pitchicon.sql | psql $db
-cat stationdirection.sql | psql $db
-cat viewpointdirection.sql | psql $db
+log "arealabel.sql OK"
 
+log "pitchicon.sql..."
+cat pitchicon.sql | psql $db
+log "pitchicon.sql OK"
+
+log "stationdirection.sql..."
+cat stationdirection.sql | psql $db
+log "stationdirection.sql OK"
+
+log "viewpointdirection.sql..."
+cat viewpointdirection.sql | psql $db
+log "viewpointdirection.sql OK"
+
+log "update_isolations_from_topographic_isolation_viefinderpanoramas.txt.sh..."
 ./update_isolations_from_topographic_isolation_viefinderpanoramas.txt.sh
+log "update_isolations OK"
+
+log "update_lowzoom_in_gis.sh..."
 ./update_lowzoom_in_gis.sh
+log "update_lowzoom_in_gis OK"
+
+log "update_parking.sh..."
 ./update_parking.sh
+log "update_parking OK"
+
+log "=== run-everything DONE ==="
